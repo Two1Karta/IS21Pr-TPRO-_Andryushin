953bb43d9f349ef84a06add4f5954adc13ec7ce1
Максим Андрюшин <maksimandru@gmail.com>
2021-10-20 16:59:45 +0300

    Обновить исходящее сообщение

---
+++ IS21Pr(TPRO)_Andryushin/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IS21Pr_TPRO__Andryushin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everyone!");
+            Console.ReadKey();
+        }
+    }
+}
